2339487f854ecde8011eba515ce3f17abfced743
appleitung <tdonder@gmail.com>
2015-03-03 16:34:57 +0100

    Watch app now submits sensitivity.
    BUT: breaks AppSync!

--- src/main_window.c
+++ src/main_window.c
@@ -3,17 +3,25 @@
 
 // BEGIN AUTO-GENERATED UI CODE; DO NOT MODIFY
 static Window *s_window;
+static GFont s_res_bitham_30_black;
+static GFont s_res_gothic_14;
 static InverterLayer *right_channel_peak;
 static InverterLayer *sensitivity_layer;
 static InverterLayer *left_channel_peak;
+static ActionBarLayer *s_actionbarlayer;
+static TextLayer *s_plusbuttonlayer;
+static TextLayer *s_minusbuttonlayer;
+static TextLayer *on_off_textlayer;
 
 static void initialise_ui(void) {
   s_window = window_create();
   window_set_background_color(s_window, GColorBlack);
   window_set_fullscreen(s_window, false);
   
+  s_res_bitham_30_black = fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK);
+  s_res_gothic_14 = fonts_get_system_font(FONT_KEY_GOTHIC_14);
   // right_channel_peak
-  right_channel_peak = inverter_layer_create(GRect(79, 1, 35, 150));
+  right_channel_peak = inverter_layer_create(GRect(70, 1, 35, 150));
   layer_add_child(window_get_root_layer(s_window), (Layer *)right_channel_peak);
   
   // sensitivity_layer
@@ -21,8 +29,35 @@ static void initialise_ui(void) {
   layer_add_child(window_get_root_layer(s_window), (Layer *)sensitivity_layer);
   
   // left_channel_peak
-  left_channel_peak = inverter_layer_create(GRect(30, 1, 35, 150));
+  left_channel_peak = inverter_layer_create(GRect(20, 1, 35, 150));
   layer_add_child(window_get_root_layer(s_window), (Layer *)left_channel_peak);
+  
+  // s_actionbarlayer
+  s_actionbarlayer = action_bar_layer_create();
+  action_bar_layer_add_to_window(s_actionbarlayer, s_window);
+  action_bar_layer_set_background_color(s_actionbarlayer, GColorWhite);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_actionbarlayer);
+  
+  // s_plusbuttonlayer
+  s_plusbuttonlayer = text_layer_create(GRect(124, 9, 20, 38));
+  text_layer_set_text(s_plusbuttonlayer, "+");
+  text_layer_set_text_alignment(s_plusbuttonlayer, GTextAlignmentCenter);
+  text_layer_set_font(s_plusbuttonlayer, s_res_bitham_30_black);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_plusbuttonlayer);
+  
+  // s_minusbuttonlayer
+  s_minusbuttonlayer = text_layer_create(GRect(124, 108, 20, 38));
+  text_layer_set_text(s_minusbuttonlayer, "-");
+  text_layer_set_text_alignment(s_minusbuttonlayer, GTextAlignmentCenter);
+  text_layer_set_font(s_minusbuttonlayer, s_res_bitham_30_black);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)s_minusbuttonlayer);
+  
+  // on_off_textlayer
+  on_off_textlayer = text_layer_create(GRect(125, 68, 18, 15));
+  text_layer_set_text(on_off_textlayer, "On");
+  text_layer_set_text_alignment(on_off_textlayer, GTextAlignmentCenter);
+  text_layer_set_font(on_off_textlayer, s_res_gothic_14);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)on_off_textlayer);
 }
 
 static void destroy_ui(void) {
@@ -30,17 +65,24 @@ static void destroy_ui(void) {
   inverter_layer_destroy(right_channel_peak);
   inverter_layer_destroy(sensitivity_layer);
   inverter_layer_destroy(left_channel_peak);
+  action_bar_layer_destroy(s_actionbarlayer);
+  text_layer_destroy(s_plusbuttonlayer);
+  text_layer_destroy(s_minusbuttonlayer);
+  text_layer_destroy(on_off_textlayer);
 }
 // END AUTO-GENERATED UI CODE
 
 static const int DISPLAY_HEIGHT = 150;
 static const int AVAILABLE_STEPS = 10;
 
+static GRect LAST_SENSITIVITY_FRAME;
+
 enum DataKeys {
   LEFT_CHANNEL_PEAK_KEY,
   RIGHT_CHANNEL_PEAK_KEY,
   LEFT_CHANNEL_AVG_KEY,
   RIGHT_CHANNEL_AVG_KEY,
+  SENSITIVITY,
 };
 
 static AppSync sync;
@@ -49,6 +91,14 @@ static uint8_t sync_buffer[256];
 static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) 
 {
   APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
+}
+
+static void out_sent_handler(DictionaryIterator *sent, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Outgoing AppMessage successfully sent.");
+}
+
+static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Outgoing AppMessage failed");
 }
 
 static int selected_sensitivity()
@@ -65,6 +115,19 @@ static void trigger_alarm(int value)
   if (value > selected_sensitivity()) {
     vibes_short_pulse();
   }
+}
+
+static void send_sensitivity()
+{
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  
+  Tuplet value = TupletInteger(SENSITIVITY, selected_sensitivity());
+  dict_write_tuplet(iter, &value);
+  
+  app_message_outbox_send();
+  
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Sensitivity sent: %d.", selected_sensitivity());
 }
 
 static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) 
@@ -99,6 +162,7 @@ static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tup
 
 static void window_load(Window *window) 
 {
+  // Prepare app_sync for sending data to phone.
   Tuplet initial_values[] = {
     TupletInteger(LEFT_CHANNEL_PEAK_KEY, (uint8_t) 0),
     TupletInteger(RIGHT_CHANNEL_PEAK_KEY, (uint8_t) 0),
@@ -106,7 +170,11 @@ static void window_load(Window *window)
     TupletInteger(RIGHT_CHANNEL_AVG_KEY, (uint8_t) 0),
   };
   app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
-      sync_tuple_changed_callback, sync_error_callback, NULL);  
+      sync_tuple_changed_callback, sync_error_callback, NULL);
+  
+  app_message_register_outbox_failed(out_failed_handler);
+  app_message_register_outbox_sent(out_sent_handler);
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
 
 static void handle_window_unload(Window* window) 
@@ -128,6 +196,8 @@ void move_sensitivity_layer(int steps)
   
   GRect newFrame = GRect(origin.x, new_y, size.w, size.h);
   layer_set_frame(layer, newFrame);
+  
+  send_sensitivity();
 }
 
 void up_single_click_handler(ClickRecognizerRef recognizer, void *context) 
@@ -138,12 +208,21 @@ void up_single_click_handler(ClickRecognizerRef recognizer, void *context)
 void select_single_click_handler(ClickRecognizerRef recognizer, void *context) 
 {
   Layer *layer = inverter_layer_get_layer(sensitivity_layer);
-  GRect frame = layer_get_frame(layer);
-  GPoint origin = frame.origin;
-  GSize size = frame.size;
-  GRect newFrame = GRect(origin.x, DISPLAY_HEIGHT / 2, size.w, size.h);
+  GRect currentFrame = layer_get_frame(layer);
+  GRect newFrame;
+  
+  if (currentFrame.origin.y >= 0 ) {
+    LAST_SENSITIVITY_FRAME = currentFrame;
+    newFrame = GRect(0, -1, 0, 0);
+    text_layer_set_text(on_off_textlayer, "Off");
+  } else {
+    newFrame = LAST_SENSITIVITY_FRAME;
+    text_layer_set_text(on_off_textlayer, "On");
+  }
   
   layer_set_frame(layer, newFrame);
+  
+  send_sensitivity();
 }
 
 void down_single_click_handler(ClickRecognizerRef recognizer, void *context) 
